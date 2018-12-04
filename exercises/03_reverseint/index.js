8d0919127107bd8216e1bc852a2e63fb2049fadc
imamachi-n <imamachigatugatu@hotmail.co.jp>
2018-12-04 22:02:36 +0900

    studying reverseInt

---
+++ exercises/03_reverseint/index.js
@@ -0,0 +1,20 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+  return Math.sign(n) * parseInt(n.toString().split('').reduce((rev, e) => e + rev, ''));
+}
+
+function reverseInt01(n) {
+  let reversed = x.toString().split('').reverse().join('');
+  return parseInt(reversed) * Math.sign(n);
+}
+
+module.exports = reverseInt;
